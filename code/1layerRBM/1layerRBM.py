d40fca83fa94df0639417b533e2bd28679702b05
qian-liu <qianliu.bjut@gmail.com>
2015-11-02 18:00:12 +0000

    reconstruction test

--- code/1layerRBM/1layerRBM.py
+++ code/1layerRBM/1layerRBM.py
@@ -1,11 +1,12 @@
 '''
 Typical usage:
-python 1layerRBM.py 1000 50 3 cd
+python -m code.1layerRBM.1layerRBM 1 50 500 pcd
+from sdbn folder
 '''
 import sys
 import random
 import numpy as np
-import poisson_tools as pt
+from ..utils import poisson_tools as pt
 import matplotlib.pyplot as plt
 from scipy.special import expit
 
@@ -43,7 +44,7 @@ batch_size = int(sys.argv[2])
 epoc = int(sys.argv[3])
 alg = sys.argv[4]
 
-random.seed(0)
+np.random.seed(0)
 
 train_x, train_y = pt.get_train_data()
 train_x = train_x > 50
@@ -70,6 +71,7 @@ eta = 0.001
 for iteration in range(epoc):
     if alg == 'pcd':
         # Persistant CD
+        print alg
         for k in range(0,train_num,batch_size):
             max_bsize = min(train_num-k, batch_size)
             data_v = Data_v[k:k+max_bsize]
@@ -94,12 +96,12 @@ for iteration in range(epoc):
             a, b, W = update_para(data_v, data_h, gibbs_v, gibbs_h, a, b, W, eta)
             print iteration+1, k+1
     
-    np.save('theta/%d_b%d_epoc%d.npy'%(train_num, batch_size, iteration+1),[a,b,W])
+    np.save('/home/liuq/apt/2ndYear/sDBN/theta/%04d_b%04d_epoc%04d_%s.npy'%(train_num, batch_size, iteration+1, alg),[a,b,W])
     data_v = np.array(train_x[index_digit[train_num]]).astype(float)
     data_h = sigmoid_sampling(data_v, W, b)
     recon = sigmoid_sampling(data_h, W.transpose(), a)
     pt.plot_digit(recon)
     #plt.draw()
-    plt.savefig('results/%d_b%d_epoc%d.pdf'%(train_num, batch_size, iteration+1))
+    plt.savefig('/home/liuq/apt/2ndYear/sDBN/results/%04d_b%04d_epoc%04d_%s.pdf'%(train_num, batch_size, iteration+1, alg))
     
 #plt.show()
