3d40bca7c8cc4e79b9066ff88ae7237ebd2fe3b8
qian-liu <qianliu.bjut@gmail.com>
2015-11-10 15:12:59 +0000

    fine tuning functions done

---
+++ code/dbn/dbn_train.py
@@ -0,0 +1,12 @@
+'''
+Deep Belief Net training
+'''
+import sys
+import random
+import numpy as np
+from ..utils import poisson_tools as pt
+import matplotlib.pyplot as plt
+from scipy.special import expit
+
+dbn_l = [784, 500, 500, 2000, 10]
+init 
